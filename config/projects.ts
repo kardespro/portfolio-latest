0f3ecf0d98ca9367d8b0218d1d1e9e5505669209
Nihad <61694826+kardespro@users.noreply.github.com>
2024-12-21 13:07:14 -0800

    Update projects.ts

--- config/projects.ts
+++ config/projects.ts
@@ -2,15 +2,15 @@ export const projects = [
   {
     title: "Discord.place",
     description: "All things related to Discord in one place.",
-    image: "https://emirhan.one/_next/image?url=%2Fimages%2Fcdn%2Fdiscordplace.png&w=1920&q=75",
+    image: "https://camo.githubusercontent.com/0b98cd5776308b04b19cf17209926032d64a190cfcccf6924ae259148ab8a6d1/68747470733a2f2f646973636f72642e706c6163652f6f672e706e67",
     link: "https://github.com/discordplace/discord.place",
     tags: ["Next.js", "Javascript", "Tailwind CSS", "Mongodb"]
   },
   {
     title: "Lantern",
     description: "Lantern makes it incredibly easy to reveal your live Discord status through a RESTful API and WebSocket connection in under 10 seconds.",
-    image: "https://emirhan.one/_next/image?url=%2Fimages%2Fcdn%2Fdiscordplace.png&w=1920&q=75",
-    link: "https://github.com/yourusername/project-two",
+    image: "https://camo.githubusercontent.com/0b98cd5776308b04b19cf17209926032d64a190cfcccf6924ae259148ab8a6d1/68747470733a2f2f646973636f72642e706c6163652f6f672e706e67",
+    link: "https://github.com/discord.place/lantern",
     tags: ["Node.js", "TypeScript", "Express"]
   }
 ]; 
